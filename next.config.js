ba7c0845f765a3b7fada12031493c3f2e4f6bdaa
Fran Mira <franm1994@gmail.com>
2023-05-09 22:08:34 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+// @ts-check
 
-module.exports = {
-  /**
-   * Tell Next.js where the `public` folder is.
-   * Replace `nextjs-github-pages` with your Github repo project name.
-   */
-  assetPrefix: isProd ? "/front-bet-scrapper/" : "",
-  /**
-   * Disable server-based image optimization.
-   *
-   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
-   */
-  images: {
-    unoptimized: true,
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '/front-bet-scrapper',
+  experimental: {
+    appDir: true,
   },
-};
+}
+
+module.exports = nextConfig
